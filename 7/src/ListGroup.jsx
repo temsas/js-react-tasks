e87cee515646a1a86cf70ccaf3ae3e9eadf59739
temsas <artemkariakin1187@gmail.com>
2025-10-05 15:54:27 +0300

    Задание 1 выполнено

--- 7/src/ListGroup.jsx
+++ 7/src/ListGroup.jsx
@@ -1,5 +1,17 @@
 import React from 'react';
 
 // BEGIN (write your solution here)
+export default class ListGroup extends React.Component{
+    render() {
+        const {children} = this.props
+        return(
+            <ul className ="list-group">
+                {React.Children.map(children, (child, i) =>{
+                    return <li className="list-group-item" key={i}>{child}</li>
+                })}
 
+            </ul>
+        )
+    }
+}
 // END
